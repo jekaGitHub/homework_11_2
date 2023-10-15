ddbcd180c5fc920a26a5e08c44653eadbf40dd46
Evgeny Safonov <jeka-luda@yandex.ru>
2023-10-15 23:17:11 +0300

    add func in file str_func and add docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_upper(str_value: str):
+"""Переводит строку в верхний регистр"""
 	return upper(str_value)
+
+
+def str_capitalize(str_value: str):
+"""Переводит первые буквы слов в верхний регистр"""
+	return title(str_value)
