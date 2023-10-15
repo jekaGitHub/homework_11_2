2a6f22d96a5cec994a06c90114debbc13ddf76d3
evgenii <jeka-luda@yandex.ru>
2023-10-15 23:47:52 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
 def str_upper(str_value: str):
 """Переводит строку в верхний регистр"""
-	return upper(str_value)
+	return str_value.upper()
 
 
 def str_capitalize(str_value: str):
 """Переводит первые буквы слов в верхний регистр"""
-	return title(str_value)
+	return str_value.title()
